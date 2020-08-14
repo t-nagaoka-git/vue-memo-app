1ba83945be56a07dc68580c44ae7e705c61a283c
Tatsuki Nagaoka <nagaoka@mbp.local>
2020-08-14 10:51:48 +0900

    Create memo app

---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <Header>My Memos</Header>
+    <ul>
+      <li v-for="memo in newest" :key="memo.id">
+        <router-link :to="{ name: 'Edit', params: { id: memo.id } }">{{ memo.body }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+textarea {
+  width: 100%;
+  height: 10em;
+}
+button {
+  border: 1px solid #333;
+  background-color: #333;
+  color: #fff;
+  padding: 10px 20px;
+  margin-top: 10px;
+}
+</style>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+  },
+  computed: {
+    newest: function () {
+      return this.$store.state.memos.slice().reverse();
+    },
+  },
+};
+</script>
---
+++ src/views/New.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="editor">
+    <Header>New Memo</Header>
+    <textarea name="memo" v-model="memoBody"></textarea>
+    <button @click="save">保存</button>
+  </div>
+</template>
+
+<style scoped>
+textarea {
+  width: 100%;
+  height: 10em;
+}
+button {
+  border: 1px solid #333;
+  background-color: #333;
+  color: #fff;
+  padding: 10px 20px;
+  margin-top: 10px;
+}
+</style>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "new",
+  components: {
+    Header,
+  },
+  data: function () {
+    return {
+      memoBody: "",
+    };
+  },
+  methods: {
+    save: function () {
+      this.$store.commit("save", {
+        body: this.memoBody,
+      });
+      this.$router.push("/");
+    },
+  },
+};
+</script>
